e6c88b539ad3f53470c838c6eeedd15ca74994d4
Ashley Lampard <ashleylampard@live.com>
2024-12-01 07:30:57 +0000

    2024 day2 setup

--- aoc_2024/common.rb
+++ aoc_2024/common.rb
@@ -1,8 +1,28 @@
-def getData(file, regex)
-  lines = File.read(file).split(regex)
-  return lines
+def get_data(file, regex)
+  File.read(file).split(regex)
 end
 
-def getRowsandColumns(file, regex)
-  getData(file, regex).map {|line| line.split("")}
+def get_rows_and_columns(file, regex)
+  getData(file, regex).map { |line| line.split('') }
+end
+
+def split_file_of_two_lists_side_by_side_seperated_by_spaces_alt(file)
+  data = File.read(file).split("\n")
+
+  array1, array2 = data.map { |line| line.split.map(&:to_i) }.transpose
+  return array1, array2
+end
+
+def split_file_of_two_lists_side_by_side_seperated_by_spaces(file)
+  data = File.read(file).split("\n")
+
+  array1 = []
+  array2 = []
+
+  data.each do |line|
+    values = line.split
+    array1 << values[0].to_i
+    array2 << values[1].to_i
+  end
+  return array1, array2
 end
---
+++ aoc_2024/day2/spec/day2_spec.rb
@@ -0,0 +1,13 @@
+require_relative "../day2"
+
+describe "part1" do
+  it "should return " do
+    expect(part1("test1.txt")).to eq(11)
+  end
+end
+
+describe "part2" do
+  it "should return " do
+    expect(part2("test2.txt")).to eq(31)
+  end
+end
