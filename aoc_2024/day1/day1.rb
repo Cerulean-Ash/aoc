de33768469dac6ca97b8c6cad84619cade6bec50
Ashley Lampard <ashleylampard@live.com>
2024-12-01 07:16:49 +0000

    2024 day1 complete

--- aoc_2024/day1/day1.rb
+++ aoc_2024/day1/day1.rb
@@ -1,15 +1,53 @@
-def part1(file)
-  file_data = File.read(file).split
+def splitFileAlt(file)
+  data = File.read(file).split("\n")
 
+  array1, array2 = data.map { |line| line.split.map(&:to_i) }.transpose
+  return array1, array2
+end
+
+def splitFile(file)
+  data = File.read(file).split("\n")
+
+  array1 = []
+  array2 = []
+
+  data.each do |line|
+    values = line.split
+    array1 << values[0].to_i
+    array2 << values[1].to_i
+  end
+  return array1, array2
+end
+
+def part1(file)
+  array1, array2 = splitFileAlt(file)
+  sortedArray1 = array1.sort
+  sortedArray2 = array2.sort
+  diffArray = []
+  sortedArray1.length.times do |i|
+    diffArray << (sortedArray1[i] - sortedArray2[i]).abs
+  end
+  diffArray.sum
 end
 
 def part2(file)
-  file_data = File.read(file).split
+  array1, array2 = splitFileAlt(file)
+  occurence_hash = {}
+  array2.each do |e|
+    occurence_hash[e] = occurence_hash[e] ? occurence_hash[e] + 1 : 1
+  end
 
+  similarity_array = []
+  array1.each do |e|
+    # similarity = occurence_hash[e] ? occurence_hash[e] : 0   - can use below instead
+    similarity = occurence_hash[e] || 0
+    similarity_array << e * similarity
+  end
+  similarity_array.sum
 end
 
-puts part1("test1.txt")
+p part1("test1.txt")
 puts part1("input.txt")
 
-puts part2("test2.txt")
+p part2("test2.txt")
 puts part2("input.txt")
--- aoc_2024/day1/spec/day1_spec.rb
+++ aoc_2024/day1/spec/day1_spec.rb
@@ -2,12 +2,12 @@ require_relative "../day1"
 
 describe "part1" do
   it "should return " do
-    expect(part1("test1.txt")).to eq(142)
+    expect(part1("test1.txt")).to eq(11)
   end
 end
 
 describe "part2" do
   it "should return " do
-    expect(part1("test2.txt")).to eq(142)
+    expect(part2("test2.txt")).to eq(31)
   end
 end
