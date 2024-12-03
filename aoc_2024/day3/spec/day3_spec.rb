3a8d8f0fd264fad1cfd3ee2ecd20459495ef3fed
Ashley Lampard <ashleylampard@live.com>
2024-12-03 06:19:56 +0000

    2024 day4 setup

--- aoc_2024/day3/spec/day3_spec.rb
+++ aoc_2024/day3/spec/day3_spec.rb
@@ -2,12 +2,12 @@ require_relative "../day3"
 
 describe "part1" do
   it "should return " do
-    expect(part1("test1.txt")).to eq(11)
+    expect(part1("test1.txt")).to eq(161)
   end
 end
 
 describe "part2" do
   it "should return " do
-    expect(part2("test2.txt")).to eq(31)
+    expect(part2("test2.txt")).to eq(48)
   end
 end
