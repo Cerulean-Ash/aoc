4c19fa37b09698a0a6ee69275f7c4d189aaead83
Ashley Lampard <ashleylampard@live.com>
2024-12-03 06:17:08 +0000

    2024 day3 done

--- aoc_2024/day3/day3.rb
+++ aoc_2024/day3/day3.rb
@@ -1,11 +1,34 @@
 def part1(file)
+  data = File.read(file).scan(/mul\(\d{1,3},\d{1,3}\)/)
+  sum = 0
+  data.each do |e|
+    numbers = e.scan(/\d+/)
+    sum += numbers[0].to_i * numbers[1].to_i
+  end
+  sum
 end
 
 def part2(file)
+  data = File.read(file).scan(/mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)/)
+  sum = 0
+  on = true
+  data.each do |e|
+    if e == "don't()"
+      on = false
+    elsif e == "do()"
+      on = true
+    end
+
+    if on
+      numbers = e.scan(/\d+/)
+      sum += numbers[0].to_i * numbers[1].to_i
+    end
+  end
+  sum
 end
 
 p part1("test1.txt")
-puts part1("input.txt")
+p part1("input.txt")
 
 p part2("test2.txt")
-puts part2("input.txt")
+p part2("input.txt")
